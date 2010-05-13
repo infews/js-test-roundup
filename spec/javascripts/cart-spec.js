42fc09405f06f57f3ec8131cfa889edb3a372cfd
Davis W. Frank <dwfrank@gmail.com>
2010-05-13 13:12:34 -0700

    interim checkin; presentation PDF

---
+++ spec/javascripts/cart-spec.js
@@ -0,0 +1,29 @@
+xdescribe("Cart", function () {
+  var cart, prod1, prod2;
+
+  beforeEach(function() {
+    prod1 = new Product({name: 'prod1', price: 10.95});
+    prod2 = new Product({name: 'prod2', price: 0.99});
+    cart = new Cart();
+  });
+
+  describe("when products are added", function () {
+    beforeEach(function() {
+      cart.add(prod1, 1);
+      cart.add(prod2, 2);
+    });
+
+    it("should have the items", function() {
+      expect(cart.itemCount).toEqual(3);
+    });
+
+    it("should update the subtotal", function() {
+      expect(cart.subtotal).toEqual(12.93);
+    });
+
+    it("should ", function() {
+      
+    });
+
+  });
+});
